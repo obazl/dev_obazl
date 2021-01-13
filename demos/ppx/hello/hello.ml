d5340906941f85c452b4f6d4c506e7996f5fbed9
Gregg Reynolds <601396+mobileink@users.noreply.github.com>
2021-01-12 20:15:55 -0600

    add demos/ppx/manual; mv demos/minimal/hello_ppx -> demos/ppx/hello

--- demos/ppx/hello/hello.ml
+++ demos/ppx/hello/hello.ml
@@ -1,10 +1,7 @@
 open Core ;;                    (* for Sexp *)
 
-(* print_endline "Hello from hello_ppx!" *)
-
 let msg =
   Sexp.to_string_hum [%sexp ([3;4;5] : int list)]
-  (* |> print_endline *)
 ;;
 
 
