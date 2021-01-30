edf1243a3db666e8513d829ea26da9b46361dab6
Gregg Reynolds <601396+mobileink@users.noreply.github.com>
2021-01-30 06:59:25 -0600

    update to rules_ocaml v1.0.0-beta.1 (new ns mech)

---
+++ demos/parsing/ocamllex/case1/main.ml
@@ -0,0 +1,15 @@
+open Core
+open Lexing
+open Lexer
+
+let scan filename () =
+  let inx = In_channel.create filename in
+  let lexbuf = Lexing.from_channel inx in
+  translate lexbuf;
+  In_channel.close inx
+
+let () =
+  Command.basic_spec ~summary:"Parse and display JSON"
+    Command.Spec.(empty +> anon ("filename" %: string))
+    scan
+  |> Command.run
