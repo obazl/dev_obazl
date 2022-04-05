f231e7dca7ba8fed4fcf4faf2bfb560d43b317b0
Gregg Reynolds <601396+mobileink@users.noreply.github.com>
2022-04-04 21:36:08 -0500

    update demos/module_binding

--- demos/module_binding/selectboth/hello_linux_fr.ml
+++
@@ -1,4 +0,0 @@
-let () = print_endline "Bonjour from hello_linux_fr.a!"
-
-let msg = "Bonjour Linux!"
-
--- demos/module_binding/selectstruct/hello_linux_en.ml
+++
@@ -1,4 +0,0 @@
-let () = print_endline "Hello from hello_linux_en.ml!"
-
-let msg = "Hello Linux!"
-
